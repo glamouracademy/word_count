c8a69ada96aeb7828cac97ebd805262ddf816857
Charlotte Chang <yellowspiderlove@gmail.com>
2015-10-17 17:11:03 -0400

    redux

--- word_count.rb
+++ word_count.rb
@@ -1,13 +1,31 @@
 class Phrase
+	
 	def initialize(phrase)
 		@phrase = phrase
 	end
 
 	def word_count
-		word_array = @phrase.scan(/\w+/)
-		words_array = word_array.group_by{ |word| word.downcase }.map { |k,v| [k, v.size] }
-		words_array.to_h
+		word_list = {}
+		@phrase.split(" ").each do |word|
+			word_list = process_words(word, word_list)
+		end
+		word_list
+	end
+
+private
+
+	def process_words(word, word_list)
+		return word_list if word.empty?
+		word_list[word] ||= 0
+		word_list[word] += 1
+		word_list
 	end
 
 end
 
+
+
+# word_array = @phrase.scan(/\w+/)
+# 		words_array = word_array.group_by{ |word| word.downcase }.map { |k,v| [k, v.size] }
+# 		test = words_array.to_h
+# 		puts test
