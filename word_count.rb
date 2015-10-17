306dc996220093cd188bd8a31f6d08401ee6ad3d
Charlotte Chang <yellowspiderlove@gmail.com>
2015-10-17 16:52:24 -0400

    first test passing

--- word_count.rb
+++ word_count.rb
@@ -1,3 +1,13 @@
 class Phrase
+	def initialize(phrase)
+		@phrase = phrase
+	end
+
+	def word_count
+		word_array = @phrase.scan(/\w+/)
+		words_array = word_array.group_by{ |word| word.downcase }.map { |k,v| [k, v.size] }
+		words_array.to_h
+	end
 
 end
+
